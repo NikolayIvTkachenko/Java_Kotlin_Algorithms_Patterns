baa0630e7e46882ad5c1e3eae95ba6b13a3ffe1b
NikolayTkachenko <tkachenni@mail.ru>
2025-05-21 02:16:51 +0300

    Update sample

---
+++ src/main/java/org/example/bit_manipulation/SampleBitProgramm001.java
@@ -0,0 +1,11 @@
+package org.example.bit_manipulation;
+
+public class SampleBitProgramm001 {
+
+    public static void main(String[] args) {
+        System.out.println("Binary: " + Integer.toString(51, 2));
+        System.out.println("Binary: " + Integer.toBinaryString(51));
+
+    }
+
+}
